80fce2cca9343479d3649fe6421d7f94d90f3fba
TNT <pxmrtai97@gmail.com>
2020-12-14 06:49:20 +0700

    add user authentication

--- src/App.js
+++ src/App.js
@@ -4,8 +4,30 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{},dispatch] = useStateValue()
+
+  useEffect(() => {
+   auth.onAuthStateChanged(authUser => {
+     console.log('USER', authUser );
+     if(authUser){
+      //if loggin or was loggin
+      dispatch({
+        type:'SET_USER',
+        user: authUser
+      })
+     }else{
+      dispatch({
+        type:'SET_USER',
+        user: null
+      })
+     }
+   })
+  }, [])
   return (
     <Router>
       <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,20 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDxnJsrkXJpb-0Q0tCD-dBmSkLTwgN5xLA",
-    authDomain: "clone-acff7.firebaseapp.com",
-    projectId: "clone-acff7",
-    storageBucket: "clone-acff7.appspot.com",
-    messagingSenderId: "180133060811",
-    appId: "1:180133060811:web:ffc47150da9a3d39f1fb4e",
-    measurementId: "G-BHYJDCNX97"
-  };
+  apiKey: "AIzaSyDxnJsrkXJpb-0Q0tCD-dBmSkLTwgN5xLA",
+  authDomain: "clone-acff7.firebaseapp.com",
+  projectId: "clone-acff7",
+  storageBucket: "clone-acff7.appspot.com",
+  messagingSenderId: "180133060811",
+  appId: "1:180133060811:web:ffc47150da9a3d39f1fb4e",
+  measurementId: "G-BHYJDCNX97",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db, auth}
